ad7e46ade69050f5250f88baf69596a7472a6221
Eugene Rush <erush91@gmail.com>
2021-08-09 10:52:36 -0600

    Works with timer callback now, but uses 20% CPU when running at 100% bag speed, need to debug.

--- include/pose_graph_compression.h
+++ include/pose_graph_compression.h
@@ -29,10 +29,17 @@ private:
   tf2_ros::Buffer tfBuffer;
   tf2_ros::TransformListener* tfListenerPtr_ = new tf2_ros::TransformListener(tfBuffer);
 
+  nav_msgs::Path path_in;
+
+  ros::Timer timerReplan_;
+  double timeIntReplan_;
+  bool init_flag = 0;
+
 public:
   pose_graph_compression(ros::NodeHandle*);
   ~pose_graph_compression();
-  
+  void setup_timer();
+  void timer_cb(const ros::TimerEvent&);
   void callback(const nav_msgs::Path::ConstPtr& msg);
   void callback2(const std_msgs::Int16MultiArray::ConstPtr& msg);
 
--- src/pose_graph_compression.cpp
+++ src/pose_graph_compression.cpp
@@ -5,94 +5,113 @@ using namespace Eigen;
 
 pose_graph_compression::pose_graph_compression(ros::NodeHandle* nh)
 {
-  pub = nh->advertise<std_msgs::Int16MultiArray>("/D01/pose_graph/compressed", 10);
-  pub2 = nh->advertise<nav_msgs::Path>("/D01/pose_graph/uncompressed", 10);
-  pub_world = nh->advertise<nav_msgs::Path>("path_world", 10);
-  sub = nh->subscribe<nav_msgs::Path>("/D01/lio_sam/mapping/path", 1, &pose_graph_compression::callback, this);
-  sub2 = nh->subscribe<std_msgs::Int16MultiArray>("/D01/pose_graph/compressed", 1, &pose_graph_compression::callback2, this);
+  nh_ = nh;
+  pub = nh_->advertise<std_msgs::Int16MultiArray>("/D01/pose_graph/compressed", 10);
+  pub2 = nh_->advertise<nav_msgs::Path>("/D01/pose_graph/uncompressed", 10);
+  pub_world = nh_->advertise<nav_msgs::Path>("path_world", 10);
+  sub = nh_->subscribe<nav_msgs::Path>("/D01/lio_sam/mapping/path", 1, &pose_graph_compression::callback, this);
+  sub2 = nh_->subscribe<std_msgs::Int16MultiArray>("/D01/pose_graph/compressed", 1, &pose_graph_compression::callback2, this);
+
+  setup_timer();
 }
+
+
+void pose_graph_compression::setup_timer()
+{
+  ROS_INFO("%s: Creating timers ...", nh_->getNamespace().c_str());
+  timerReplan_ = nh_->createTimer(ros::Duration(1.0), &pose_graph_compression::timer_cb, this);
+}
+
+
+void pose_graph_compression::timer_cb(const ros::TimerEvent&) // running at a fast rate
+{
+  if(init_flag)
+  {
+    cout << "-------------------------------------" << endl;
+    std_msgs::Int16MultiArray arr;
+    nav_msgs::Path path;
+    path.header.stamp = ros::Time::now();
+    path.header.frame_id = path_in->header.frame_id;
+    geometry_msgs::PoseStamped pose;
+    float x, y, z;
+    float x_last, y_last, z_last;
+    float xyz_distance;
+    int x_short_int, y_short_int, z_short_int;
+    int i = 0;
+    for(vector<geometry_msgs::PoseStamped>::const_iterator it = path_in->poses.begin(); it != path_in->poses.end(); it++)
+    {
+      x = path_in->poses[i].pose.position.x;
+      y = path_in->poses[i].pose.position.y;
+      z = path_in->poses[i].pose.position.z;
+      xyz_distance  = (x - x_last) * (x - x_last)
+                    + (y - y_last) * (y - y_last)
+                    + (z - z_last) * (z - z_last);
+      if(xyz_distance > 25)
+      {
+        path.poses.push_back(*it);
+        x_last = x;
+        y_last = y;
+        z_last = z;
+      }
+      i++;
+    }
+    cout << "ORIGINAL FLOAT:        [" << x << ", " << y << ", " << z << "]" << endl;
+    cout << "LAST:                  [" << x_last << ", " << y_last << ", " << z_last << "]" << endl;
+
+    nav_msgs::Path pathMsgOut;
+    pathMsgOut.header.frame_id = "world";
+    pathMsgOut.header.stamp = ros::Time::now();
+    pathMsgOut.poses.resize(path.poses.size());
+
+    if(pathMsgOut.poses.size() < 1)
+    {
+      pub_world.publish(pathMsgOut);
+      return;
+    }
+
+    geometry_msgs::TransformStamped pathInToOut;
+    try
+    {
+      pathInToOut = tfBuffer.lookupTransform(pathMsgOut.header.frame_id, path.header.frame_id, ros::Time(0));
+    }
+    catch(tf2::TransformException &ex)
+    {
+      ROS_WARN("%s",ex.what());
+      return;
+    }
+
+    for(int i=0; i<pathMsgOut.poses.size(); i++)
+      tf2::doTransform (path.poses[i].pose, pathMsgOut.poses[i].pose, pathInToOut);
+    
+    pub_world.publish(pathMsgOut);
+
+
+    int j = 0;
+    for(vector<geometry_msgs::PoseStamped>::const_iterator it = pathMsgOut.poses.begin(); it != pathMsgOut.poses.end(); it++)
+    {
+      x = pathMsgOut.poses[j].pose.position.x;
+      y = pathMsgOut.poses[j].pose.position.y;
+      z = pathMsgOut.poses[j].pose.position.z;
+
+      x_short_int = static_cast<short int>(20 * x);
+      y_short_int = static_cast<short int>(20 * y);
+      z_short_int = static_cast<short int>(20 * z);
+
+      arr.data.push_back(x_short_int);
+      arr.data.push_back(y_short_int);
+      arr.data.push_back(z_short_int);
+      j++;
+    }
+    cout << "COMPRESSED SHORT INT:  [" << x_short_int << ", " << y_short_int << ", " << z_short_int << "]" << endl;
+    pub.publish(arr);
+  }
+}
+
 
 void pose_graph_compression::callback(const nav_msgs::Path::ConstPtr& msg)
 {
-
-  cout << "-------------------------------------" << endl;
-  std_msgs::Int16MultiArray arr;
-  nav_msgs::Path path;
-  path.header.stamp = ros::Time::now();
-  path.header.frame_id = msg->header.frame_id;
-  geometry_msgs::PoseStamped pose;
-  float x, y, z;
-  float x_last, y_last, z_last;
-  float xyz_distance;
-  int x_short_int, y_short_int, z_short_int;
-  int i = 0;
-  for(vector<geometry_msgs::PoseStamped>::const_iterator it = msg->poses.begin(); it != msg->poses.end(); it++)
-  {
-    x = msg->poses[i].pose.position.x;
-    y = msg->poses[i].pose.position.y;
-    z = msg->poses[i].pose.position.z;
-    xyz_distance  = (x - x_last) * (x - x_last)
-                  + (y - y_last) * (y - y_last)
-                  + (z - z_last) * (z - z_last);
-    if(xyz_distance > 25)
-    {
-      path.poses.push_back(*it);
-      x_last = x;
-      y_last = y;
-      z_last = z;
-    }
-    i++;
-  }
-  cout << "ORIGINAL FLOAT:        [" << x << ", " << y << ", " << z << "]" << endl;
-  cout << "LAST:                  [" << x_last << ", " << y_last << ", " << z_last << "]" << endl;
-
-  nav_msgs::Path pathMsgOut;
-  pathMsgOut.header.frame_id = "world";
-  pathMsgOut.header.stamp = ros::Time::now();
-  pathMsgOut.poses.resize(path.poses.size());
-
-  if(pathMsgOut.poses.size() < 1)
-  {
-    pub_world.publish(pathMsgOut);
-    return;
-  }
-
-  geometry_msgs::TransformStamped pathInToOut;
-  try
-  {
-    pathInToOut = tfBuffer.lookupTransform(pathMsgOut.header.frame_id, path.header.frame_id, ros::Time(0));
-  }
-  catch(tf2::TransformException &ex)
-	{
-		ROS_WARN("%s",ex.what());
-    return;
-	}
-
-  for(int i=0; i<pathMsgOut.poses.size(); i++)
-    tf2::doTransform (path.poses[i].pose, pathMsgOut.poses[i].pose, pathInToOut);
-	
-	pub_world.publish(pathMsgOut);
-
-
-  int j = 0;
-  for(vector<geometry_msgs::PoseStamped>::const_iterator it = pathMsgOut.poses.begin(); it != pathMsgOut.poses.end(); it++)
-  {
-    x = pathMsgOut.poses[j].pose.position.x;
-    y = pathMsgOut.poses[j].pose.position.y;
-    z = pathMsgOut.poses[j].pose.position.z;
-
-    x_short_int = static_cast<short int>(20 * x);
-    y_short_int = static_cast<short int>(20 * y);
-    z_short_int = static_cast<short int>(20 * z);
-
-    arr.data.push_back(x_short_int);
-    arr.data.push_back(y_short_int);
-    arr.data.push_back(z_short_int);
-    j++;
-  }
-  cout << "COMPRESSED SHORT INT:  [" << x_short_int << ", " << y_short_int << ", " << z_short_int << "]" << endl;
-  pub.publish(arr);
-    
+  path_in = msg;
+  init_flag = 1;
 }
 
 void pose_graph_compression::callback2(const std_msgs::Int16MultiArray::ConstPtr& msg)
